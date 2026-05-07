50fe4d5c590d82c2c78715c08793ae67016287e2
Gift Jacksun <codewithjacksun@gmail.com>
2026-05-07 10:20:04 +0100

    feat: update GTranslate component to include German in language options for enhanced translation support

--- frontend/src/components/main/language-translator.tsx
+++ frontend/src/components/main/language-translator.tsx
@@ -68,7 +68,7 @@ const GTranslate = () => {
     window.gtranslateSettings = {
       default_language: "en",
       detect_browser_language: true,
-      languages: ["en", "fr", "es", "ru", "sv"],
+      languages: ["en", "fr", "es", "de", "ru", "sv"],
       wrapper_selector: ".gtranslate_wrapper",
       switcher_horizontal_position: "inline",
       float_switcher_open_direction: "bottom",
